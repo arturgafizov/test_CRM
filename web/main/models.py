af255e2776eae31619779a8205e1ff91cd8d7092
Artur <art05xxx@rambler.ru>
2022-01-17 00:26:26 +0300

    3 commit update User model

--- web/main/models.py
+++ web/main/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 from .managers import UserManager
 
 
 class User(AbstractUser):
 
+    class StatusChoice(models.TextChoices):
+        CLIENT = ('client', 'Client')
+        EMPLOYEE = ('employee', 'Employee')
+
     username = None
     email = models.EmailField(_('Email address'), unique=True)
-
+    status = models.CharField(max_length=8, choices=StatusChoice.choices)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
+    phone = PhoneNumberField(null=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
